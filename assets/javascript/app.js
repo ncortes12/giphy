3b088faaf59c9c1e6e2972fe0ba73955b6b185a4
ncortes <ncortes1415@gmail.com>
2018-06-06 20:06:03 -0700

    6/6/18 updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -34,42 +34,58 @@ function displayGiphy() {
         method: "GET"
     }).then(function (response) {
         console.log(response)
-        
-        var results= response.data
-    for (var i=0; i<results.length; i++){
-        var gifDiv = $("<div>");
-        var p = $("<p>").text("Rating: "+ results[i].rating);
-        var favorite = $("<button>").text("Favorite")
-        favorite.addClass("favorite");
-        var image = $("<img>");
-        image.attr("src", results[i].images.fixed_height_still.url);
-        image.attr("data-still", results[i].images.fixed_height_still.url);
-        image.attr("data-animate", results[i].images.fixed_height.url);
-        image.attr("state", "still");
-        image.addClass("image");
-        gifDiv.append(p);
-        gifDiv.append(image);
-        gifDiv.append(favorite);
-        $("#giph-here").prepend(gifDiv);
+
+        var results = response.data
+        for (var i = 0; i < results.length; i++) {
+            var gifDiv = $("<div>");
+            gifDiv.addClass("display");
+            var p = $("<p>").text("Rating: " + results[i].rating);
+            var p2 = $("<p>").text("Title: " + results[i].title);
+            var p3 = $("<p>").text("Source: " + results[i].source_tld);
+            var favorite = $("<button>").text("Favorite")
+            favorite.addClass("favorite");
+            var image = $("<img>");
+            image.attr("src", results[i].images.fixed_height_still.url);
+            image.attr("data-still", results[i].images.fixed_height_still.url);
+            image.attr("data-animate", results[i].images.fixed_height.url);
+            image.attr("state", "still");
+            image.addClass("image");
+            gifDiv.append(p);
+            gifDiv.append(p2);
+            gifDiv.append(p3);
+            gifDiv.append(image);
+            gifDiv.append(favorite);
+            $("#giph-here").prepend(gifDiv);
 
 
 
-    }
-$(".image").on("click",function (){
-    var state = $(this).attr("state");
-
-        if (state === "still") {
-          $(this).attr("src", $(this).attr("data-animate"));
-          $(this).attr("state", "animate");
-        } else {
-          $(this).attr("src", $(this).attr("data-still"));
-          $(this).attr("state", "still");
         }
+        $(".image").on("click", function () {
+            var state = $(this).attr("state");
+
+            if (state === "still") {
+                $(this).attr("src", $(this).attr("data-animate"));
+                $(this).attr("state", "animate");
+            } else {
+                $(this).attr("src", $(this).attr("data-still"));
+                $(this).attr("state", "still");
+            }
+        })
+
+        $(".favorite").on("click", function () {
+            console.log("yay")
+            $("#favorites").append(gifDiv);
+            var storeRating = JSON.stringify(p);
+            var storeImage = JSON.stringify(image);
+            localStorage.setItem("rating",storeRating);
+            localStorage.setItem("image",storeImage);
+           
 
 
-})
-        
+        })
+
     })
 }
 $(document).on("click", ".animal", displayGiphy);
+
 
